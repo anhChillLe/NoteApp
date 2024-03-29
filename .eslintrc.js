73d450d75236ccecb1adb4bdce25c0374988d89d
Lê Anh Chiêu <chieu280200@gmail.com>
2024-03-29 21:41:48 +0700

    eslint update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
-};
+  extends: ['@react-native', 'prettier'],
+  rules: {
+    semi: 0,
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    '@typescript-eslint/no-shadow': ['off'],
+    'react-native/no-inline-styles': 'warn',
+  },
+}
