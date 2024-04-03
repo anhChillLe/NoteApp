bdbfce3203f28acd4ad430531744da396c9cd2df
Lê Anh Chiêu <chieu280200@gmail.com>
2024-04-03 10:21:32 +0700

    Fix reanimated babel warning, remove npm

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 const presets = ['module:@react-native/babel-preset']
 const reanimated = ['react-native-reanimated/plugin']
-const babelDecorator = ["@babel/plugin-proposal-decorators", { legacy: true }]
-const plugins = [reanimated, babelDecorator]
+const babelDecorator = ['@babel/plugin-proposal-decorators', { legacy: true }]
+const plugins = [babelDecorator, reanimated]
 
 module.exports = {
   presets,
