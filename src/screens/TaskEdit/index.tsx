49cb720a797e08e734250070009e3496a3da9c49
anhChillLe <chieu280200@gmail.com>
2024-05-01 23:13:54 +0700

    [Update] Change model method

--- src/screens/TaskEdit/index.tsx
+++ src/screens/TaskEdit/index.tsx
@@ -4,7 +4,7 @@ import { BSON } from 'realm'
 import { TaskEditLayout, TaskForm } from '~/components/templates'
 import { useRootRoute } from '~/navigation/root/hook'
 import { useObject, useQuery, useRealm } from '~/services/database'
-import { Tag, Task } from '~/services/database/model'
+import { Note, Tag } from '~/services/database/model'
 
 export const TaskEditScreen: FC = () => {
   const route = useRootRoute<'task_edit'>()
@@ -12,18 +12,18 @@ export const TaskEditScreen: FC = () => {
   const realm = useRealm()
   const tags = useQuery(Tag)
   const [id, setId] = useState(new BSON.UUID(route.params?.id))
-  const task = useObject(Task, id)
+  const task = useObject(Note, id)
 
   const handleDataChange = useCallback(
     (data: TaskForm) => {
-      if (!data.title && !data.items?.length) return
+      if (!data.title && !data.taskList?.length) return
       realm.write(() => {
-        const task = realm.objectForPrimaryKey(Task, id)
+        const task = realm.objectForPrimaryKey(Note, id)
 
         if (task != null) {
           task.update(data)
         } else {
-          const { _id } = realm.create(Task, Task.generate(data))
+          const { _id } = realm.create(Note, Note.generateTask(data))
           setId(_id)
         }
       })
