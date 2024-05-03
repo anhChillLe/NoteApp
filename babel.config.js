d1bc415a5a33172c04fcb80d46bfbd3726ff7756
anhChillLe <chieu280200@gmail.com>
2024-05-03 14:52:13 +0700

    [Reset] Revert to RN 0.73.6, clean deps

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,5 @@
 const presets = ['module:@react-native/babel-preset']
-const reanimated = ['react-native-reanimated/plugin']
-const babelDecorator = ['@babel/plugin-proposal-decorators', { legacy: true }]
-const plugins = [babelDecorator, reanimated]
+const plugins = ['react-native-reanimated/plugin']
 
 module.exports = {
   presets,
